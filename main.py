c87eb71da8d43fbdbce8e38f4ea6095df32917e2
Sean R Reid <sean@torchcodelab.com>
2024-02-09 15:00:19 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from billboard import ChartData
+
+def getChart(year):
+    chart = ChartData("hot-100-songs", year=year)
+    return chart
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    hot_100 = getChart(2009)
+    return {"json": hot_100}
+
+
+@app.get("/hot-100/{year}")
+def read_item(year: str):
+    print(year)
+    hot_100 = getChart(year)
+    return {"json": hot_100}
